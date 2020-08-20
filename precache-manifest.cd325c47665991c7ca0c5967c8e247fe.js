006efbf6ce80ecd6e906e35ff7cac752e8c2b481
Andriy007 <andrii.zhukovych@zazmic.com>
2020-08-20 19:05:13 +0300

    Updates

---
+++ precache-manifest.cd325c47665991c7ca0c5967c8e247fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77adff1fe41bdb5da350229ae7761e20",
+    "url": "/currentWeather/index.html"
+  },
+  {
+    "revision": "a3089bb4cdd829a45f60",
+    "url": "/currentWeather/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "0a9fd30fadbdddce6997",
+    "url": "/currentWeather/static/css/main.25f2fc85.chunk.css"
+  },
+  {
+    "revision": "a3089bb4cdd829a45f60",
+    "url": "/currentWeather/static/js/2.a49a0cab.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/currentWeather/static/js/2.a49a0cab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a9fd30fadbdddce6997",
+    "url": "/currentWeather/static/js/main.c997fb75.chunk.js"
+  },
+  {
+    "revision": "e49f06750597d5ab9134",
+    "url": "/currentWeather/static/js/runtime-main.6f53e298.js"
+  }
+]);
